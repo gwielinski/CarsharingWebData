da87dedccc5b2207af70ab669834374da9fa8f2c
gwielinski <g.wielinski@hotmail.ca>
2016-09-07 15:28:53 -0400

    Update createODDf.R

--- R/createODDf.R
+++ R/createODDf.R
@@ -2,8 +2,9 @@ createODDf <- function(df, interval = 300){
 
   od <- df %>%
           select(vin, model_name, id, nextobs, lat, lon, nextlat, nextlon, ymdhm, nextymdhm, energy_level, nextenergy_level,
-                 timediff, energydiff, nextyear, nextmonth, nextday, nextwday) %>%
+                 timediff, energydiff, nextyear, nextmonth, nextday, nextwday, nexthour) %>%
           filter(timediff > interval)
 
   return(od)
 }
+
