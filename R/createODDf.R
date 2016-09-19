07435c95bae01fc8426cc035dfc5b20dea791264
gwielinski <g.wielinski@hotmail.ca>
2016-09-19 08:23:52 -0400

    Add parameter "component"

--- R/createODDf.R
+++ R/createODDf.R
@@ -1,10 +1,25 @@
-createODDf <- function(df, interval = 300){
+createODDf <- function(df, interval = 300, component = "Destination"){
 
-  od <- df %>%
+  # Si on choisit de garder les données sur la destination de la transacton:
+ if(component="Destination"){
+   od <- df %>%
           select(vin, model_name, id, nextobs, lat, lon, nextlat, nextlon, ymdhm, nextymdhm, energy_level, nextenergy_level,
                  timediff, energydiff, nextyear, nextmonth, nextday, nextwday, nexthour) %>%
           filter(timediff > interval)
 
   return(od)
+ }
+  # Si on choisit de garder les données sur l'origine de la transacton:
+  if(component="Origine"){
+    od <- df %>%
+      select(vin, model_name, id, nextobs, lat, lon, nextlat, nextlon, ymdhm, nextymdhm, energy_level, nextenergy_level,
+             timediff, energydiff, year, month, day, wday, hour) %>%
+      filter(timediff > interval)
+
+    return(od)
+  }else{
+    return("The component parameter is not set properly.")
+  }
+
 }
 
