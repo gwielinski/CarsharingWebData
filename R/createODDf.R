2183d9c6a6b233ebe4579fc31f0ea32d0e2dea1a
gwielinski <g.wielinski@hotmail.ca>
2016-09-19 08:24:52 -0400

    Update createODDf.R

--- R/createODDf.R
+++ R/createODDf.R
@@ -1,7 +1,7 @@
 createODDf <- function(df, interval = 300, component = "Destination"){
 
   # Si on choisit de garder les données sur la destination de la transacton:
- if(component="Destination"){
+ if(component=="Destination"){
    od <- df %>%
           select(vin, model_name, id, nextobs, lat, lon, nextlat, nextlon, ymdhm, nextymdhm, energy_level, nextenergy_level,
                  timediff, energydiff, nextyear, nextmonth, nextday, nextwday, nexthour) %>%
@@ -10,7 +10,7 @@ createODDf <- function(df, interval = 300, component = "Destination"){
   return(od)
  }
   # Si on choisit de garder les données sur l'origine de la transacton:
-  if(component="Origine"){
+  if(component=="Origine"){
     od <- df %>%
       select(vin, model_name, id, nextobs, lat, lon, nextlat, nextlon, ymdhm, nextymdhm, energy_level, nextenergy_level,
              timediff, energydiff, year, month, day, wday, hour) %>%
